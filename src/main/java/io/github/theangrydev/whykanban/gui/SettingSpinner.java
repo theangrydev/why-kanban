691b0eec8e0d71884dcd13d7ed43258c6bd48f26
Liam Williams <liam.williams@zoho.com>
2016-02-02 22:18:25 +0000

    Change spinner value using the keyboard

--- src/main/java/io/github/theangrydev/whykanban/gui/SettingSpinner.java
+++ src/main/java/io/github/theangrydev/whykanban/gui/SettingSpinner.java
@@ -1,6 +1,8 @@
 package io.github.theangrydev.whykanban.gui;
 
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import org.reactfx.EventStream;
@@ -14,7 +16,17 @@ public class SettingSpinner extends GridPane {
 	private SettingSpinner(String teamMemberType, int initialValue) {
 		add(new Text(teamMemberType), 0, 0);
 		spinner = new Spinner<>(0, 100, initialValue);
+		makeEditable(initialValue);
 		add(spinner, 0, 1);
+	}
+
+	private void makeEditable(int initialValue) {
+		SpinnerValueFactory<Integer> factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, initialValue);
+		spinner.setValueFactory(factory);
+		TextFormatter<Integer> formatter = new TextFormatter<>(factory.getConverter(), factory.getValue());
+		spinner.getEditor().setTextFormatter(formatter);
+		factory.valueProperty().bindBidirectional(formatter.valueProperty());
+		spinner.setEditable(true);
 	}
 
 	public static SettingSpinner settingSpinner(String teamMemberType, int initialValue) {
